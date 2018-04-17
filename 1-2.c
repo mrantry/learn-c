d60da68551386ea0063f2784d46ad65020464920
Michael Antry <antry2@illinois.edu>
2018-04-16 20:42:46 -0500

    doing some basic maths

---
+++ 1-2.c
@@ -0,0 +1,21 @@
+/* A simple temperture converter
+Prints fahrenheit tempertures and their celsius conversions in a table format
+For temperatures 0-300 degrees fahrenheit in increments of 20 */
+#include <stdio.h>
+
+main()
+{
+  int fahr, celsius;
+  int lower, upper, step;
+  
+  lower = 0; /* Lower limit of the temp table */
+  upper = 300; /* Upper limit of the temp table */
+  step = 20; /* Step size */
+  
+  fahr = lower;
+  while (fahr <= upper) {
+    celsius = 5 * (fahr-32) / 9;
+    printf("%d\t%d\n", fahr, celsius);
+    fahr = fahr + step;
+  }
+}
